78c096013706dc297ccba26ecbb6320358ff9113
Angel Grullon Office <agrullon@prosertrd.com>
2024-10-29 10:38:04 -0400

    Removed empleado from Cursos

--- apps/empleado/applications/persona/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('listar-todo-empleados/', views.ListAllEmployees.as_view()),
-]
--- apps/empleado/applications/persona/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-
-# models
-from .models import Empleado
-
-class ListAllEmployees(ListView):
-    template_name = 'persona/list_all.html'
-    Model = Empleado
