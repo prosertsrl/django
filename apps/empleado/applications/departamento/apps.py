2a7655f2a7318fb9ab35a7e08cd400257701eee7
Angel Grullon Office <agrullon@prosertrd.com>
2024-10-29 14:52:03 -0400

    Removed empleado

--- apps/empleado/applications/departamento/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DepartamentoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'applications.departamento'
--- apps/empleado/applications/home/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('home/', views.IndexView.as_view()),
-    path('lista/', views.PruebaListView.as_view()),
-    path('lista-prueba/', views.ModelPruebaListView.as_view()),
-]
