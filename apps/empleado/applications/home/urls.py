40a6a11ecaeff01b3a7042ff3139ca7fd37f634c
Angel Grullon Office <agrullon@prosertrd.com>
2024-10-29 14:50:00 -0400

    Removed empleado directory

---
+++ apps/empleado/applications/home/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('home/', views.IndexView.as_view()),
+    path('lista/', views.PruebaListView.as_view()),
+    path('lista-prueba/', views.ModelPruebaListView.as_view()),
+]
